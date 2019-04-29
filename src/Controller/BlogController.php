c62f6c172918fbd26bc9728503276fbdb1b99dfe
mohamed taher grati <mohamedtahergrati@yahoo.fr>
2019-04-29 11:36:41 -0400

    add authentication

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use App\Form\ArticleType;
+use App\Entity\Comment;
+use App\Form\CommentType;
 
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -74,10 +76,26 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(Article $article){
+    public function show(Article $article, Request $request, ObjectManager $manager){
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id'=> $article->getId()]);
+        }
 
         return $this->render('blog/show.html.twig',[
-            'article' => $article
+            'article' => $article,
+            'commentForm' => $form->createView()
         ]);
     }
 
